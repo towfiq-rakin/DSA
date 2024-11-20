d9c5e3d1c5de1c8384c73d98d37847ecac07b2dd
Towfiq Omar Rakin <towfiqomarrakin@gmail.com>
2024-11-20 19:29:41 +0600

    stack.cpp
    This file defines a generic stack class using a singly linked list in C++. The stack supports typical stack operations such as push, pop, and top, and includes additional utility functions like print, empty, and size.
    
    Key Components
    
    Node Structure:
    A nested structure Node that holds the data of type T and a pointer to the next node.
    Member Variables:
    
    Node* head: Pointer to the top of the stack.
    int count: Keeps track of the number of elements in the stack.
    Constructor: Initializes head to nullptr and count to 0.
    
    Methods:
    void print(): Prints all elements in the stack. Throws an underflow_error if the stack is empty.
    
    void push(T data): Adds a new element to the top of the stack.
    
    T top(): Returns the top element of the stack. Throws an underflow_error if the stack is empty.
    
    void pop(): Removes the top element of the stack. Throws an underflow_error if the stack is empty.
    
    bool empty() const: Checks if the stack is empty.
    
    int size() const: Returns the number of elements in the stack.

---
+++ stack.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+#define endl "\n"
+template <typename T>
+
+class stack{
+    struct Node{
+        T data;
+        Node* next;
+    };
+    Node* head;
+    int count;
+
+    public:
+    stack() : head(nullptr), count(0) {} // Constructor
+
+    // Print the stack
+    void print(){
+        if(empty()){
+            throw underflow_error("Stack is empty");
+        }
+        Node* temp = head;
+        while(temp != nullptr){
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        }
+        cout<<endl;
+    }
+    // Push the element at the top of the stack
+    void push(T data){
+        Node *newnode = new Node;
+        newnode->data = data;
+        newnode->next = head;
+        head = newnode;
+        ++count;
+        return;
+    }
+    // Return the top element of the stack
+    T top(){
+        if(empty()){
+            throw underflow_error("Stack is empty");
+        }
+        return head->data;
+    }
+
+    // Remove the top element of the stack
+    void pop(){
+        if(empty()){
+            throw underflow_error("Stack is empty");
+        }
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        --count;
+    }
+
+    // Check if the stack is empty
+    bool empty(){
+        return head == nullptr;
+    }
+
+    // Return the number of elements in the stack
+    int size(){
+        if(empty()) throw "Stack is empty";
+        return count;
+    }
+};
+
+int main(){
+    stack<int> s;
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    cout << "Enter the elements: ";
+    for(int i=0;i<n;i++){
+        int x; cin>>x;
+        s.push(x);
+    }
+
+    cout << "The stack is: ";
+    s.print();
+    cout << "The top element is: " << s.top() << endl;
+    s.pop();
+    cout << "After popping the top element, the stack is: ";
+    s.print();
+    cout << "The number of elements in the stack is: " << s.size() << endl;
+    return 0;
+}
