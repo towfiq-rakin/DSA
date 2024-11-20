9845675bd373843a35f52e833a8bf52631ff96c1
Towfiq Omar Rakin <towfiqomarrakin@gmail.com>
2024-11-20 20:09:31 +0600

    Create valid_parentheses.cpp

---
+++ valid_parentheses.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+#define endl "\n"
+
+// Check if the parentheses are balanced
+bool solve(string p){
+    stack<char> b; // Stack to store the opening parentheses
+    
+    for(char c: p){
+        if(c=='{' || c=='(' || c=='[') 
+            b.push(c); // Push the opening parentheses
+        else{
+            if(b.empty()) return false; // return false if there is no opening parentheses
+
+            if(c == ')' && b.top()=='('){
+                b.pop();
+            }else if(c == '}' && b.top()=='{'){
+                b.pop();
+            }else if(c == ']' && b.top()=='['){
+                b.pop();
+            }
+            else{
+                return false;
+            }
+        }
+    }
+    return true;
+}
+int main(){
+    string s;
+    cout << "Enter the string of parentheses: ";
+    getline(cin, s);
+    cout << (solve(s) ? "Balanced" : "Not Balanced") << endl;
+    return 0;
+}
