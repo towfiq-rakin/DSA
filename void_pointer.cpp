0a4c5cc567e407c7c0c3904c46fcabe59a4c3900
Towfiq Omar Rakin <towfiqomarrakin@gmail.com>
2024-12-04 20:00:08 +0600

    Create void_pointer.cpp

---
+++ void_pointer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void increase(void *data, int psize){
+    if ( psize == sizeof(char) ){  // check for char
+        char *pchar; 
+        pchar=(char*)data; 
+        ++(*pchar); 
+    }
+    else if (psize == sizeof(int)){ // Check for int
+        int *pint; 
+        pint=(int*)data; 
+        ++(*pint); 
+    }
+}
+int main (){
+    char a = 'x';
+    int b = 1602;
+    increase (&a, sizeof(a));
+    increase (&b, sizeof(b));
+    cout << a << " " << b << endl;
+}
